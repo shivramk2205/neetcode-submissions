8788bb96d5bd449844522a0ff0b802d6a4278462
Shivram K <23i262@psgtech.ac.in>
2026-07-22 11:52:14 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mp1={}
+        mp2={}
+        for i in s:
+            mp1[i]=mp1.get(i,0)+1
+        for i in t:
+            mp2[i]=mp2.get(i,0)+1
+        return mp1==mp2
+        
